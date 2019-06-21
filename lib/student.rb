acb48fd123e090389be1384356a95bb849444ec6
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 18:24:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
       WHERE grade < 12
     SQL
     binding.pry
-    DB[:conn].execute(sql, '12').map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
