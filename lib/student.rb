829446452d2a5d4e174d3cbb14d9fd001ea561f6
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 18:29:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,9 +54,9 @@ class Student
   #   self.all.select{|student| student.grade.to_i < 12}
   # end
 
-  #def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10(x)
     #self.all.select.with_index(1){|student, i| student.grade.to_i == 10 && i <= X}
-  #end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
