a9be0ed5383c1ea3452e3c6449567f3233ac238b
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 18:22:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,25 +23,29 @@ class Student
     end
   end
 
-  # def self.all_students_in_grade_9
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE grade = ?
-  #   SQL
-  #   binding.pry
-  #   DB[:conn].execute(sql, "9").map do |row|
-  #     self.new_from_db(row)
-  #   end
-  # end
-
   def self.all_students_in_grade_9
-    self.all.select{|student| student.grade.to_i == 9}
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    binding.pry
+    DB[:conn].execute(sql, "9").map do |row|
+      self.new_from_db(row)
+    end
   end
+
+  # def self.all_students_in_grade_9
+  #   self.all.select{|student| student.grade.to_i == 9}
+  # end
 
   def self.students_below_12th_grade
     self.all.select{|student| student.grade.to_i < 12}
   end
+
+  # def self.students_below_12th_grade
+  #   self.all.select{|student| student.grade.to_i < 12}
+  # end
 
   def self.find_by_name(name)
     sql = <<-SQL
