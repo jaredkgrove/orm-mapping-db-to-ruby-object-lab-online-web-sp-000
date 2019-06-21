fa71d3ac80a6e7ccf1a147b958ebe72086b7f338
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 17:53:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,9 @@ class Student
       SELECT *
       FROM students
     SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_name(name)
