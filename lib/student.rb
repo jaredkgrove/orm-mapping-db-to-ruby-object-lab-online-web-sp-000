7858f547c3c296cd6b083846bd16a7d29fab8485
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 17:57:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,11 @@ class Student
   end
 
   def self.all_students_in_grade_9
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
   end
 
   def self.find_by_name(name)
