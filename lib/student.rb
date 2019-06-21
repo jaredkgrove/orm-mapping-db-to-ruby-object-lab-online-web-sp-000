6aaecf86a0d9b5d0ae09f43377c6f5a1d5a89192
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 18:34:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,18 @@ class Student
   # end
 
   def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
     self.all.select{|student| student.grade.to_i == grade}
   end
 
